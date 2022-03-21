06fd58cf6a23fdae289c5f7a4b0697e4aff85bf9
AustinProphet <austinprophet@gmail.com>
2022-03-21 19:33:46 -0400

    added duration of use feature, updated ReadMe

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -4,14 +4,14 @@
     {
         static void Main(string[] args)
         {
-            var time = DateTime.Now.ToString(format: "hh:mm:ss tt");
+            var starttime = DateTime.Now;
             bool exitLoop = false;
 
             do
             {
                 Console.WriteLine("Welcome to My Calculator!");
-                Console.WriteLine("The Current Time Is");
-                Console.WriteLine(time);
+                Console.WriteLine("You Began Using My Calculator at " + starttime);
+                Console.WriteLine("\n");
                 Console.WriteLine("Enter the action to be performed");
                 Console.WriteLine("Press 1 for Addition");
                 Console.WriteLine("Press 2 for Subtraction");
@@ -62,12 +62,16 @@
                             feet = Convert.ToInt32(Console.ReadLine());
                             meter = feet / 3.2808399;
                             Console.WriteLine("\nFeet in meter : " + meter);
+                            Console.WriteLine("Press Enter To Continue \n");
                             Console.ReadKey();
                             break;
                         }
                     case 6:
                         {
-                            Console.WriteLine("Thanks for using My Calculator. Goodbye!");
+                            var endtime = DateTime.Now.Subtract(starttime);
+                            Console.WriteLine("Thanks for using My Calculator!");
+                            Console.WriteLine("You have been using My Calculator for " + endtime);
+                            Console.WriteLine("Goodbye!");
                             exitLoop = true;
                             break;
                         }
@@ -79,6 +83,7 @@
                 if (action != 6 && action != 5)
                 {
                     Console.WriteLine("The result is {0} \n", result);
+                    Console.WriteLine("Press Enter to continue \n");
                     Console.ReadKey();
                 }
             } while (!exitLoop);
--- BasicCalculator/README.md
+++ BasicCalculator/README.md
@@ -1,4 +1,4 @@
 # BasicCalculator2022
 A Calculator:
 This app will combine 2 numerical user inputs using the traditional mathematic modifiers: addition, subtraction, multiplication, and division.
-Additionally this calculator will also convert Feet to meters.
+Additionally this calculator will also convert Feet to meters as well as tell you how long you have been using the program upon exit.
